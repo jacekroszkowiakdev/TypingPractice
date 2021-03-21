18692b7c54e935470321625896880f99f0b158f7
Jacek Roszkowiak <jacekroszkowiakdev@gmail.com>
2021-03-21 17:52:00 +0100

    changes

--- src/components/TypingPractice/index.tsx
+++ src/components/TypingPractice/index.tsx
@@ -1,7 +1,20 @@
 import "./TypingPractice.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { TypingPracticeContext } from "../../App";
 
 export const TypingPractice = () => {
     console.log("typing practice component did mount");
-    return <div></div>;
+    const [time, setTime] = useState<number>(0);
+    const userText = document.getElementById("userText");
+
+    userText.addEventListener("input", function () {
+        var userInput = userText.value;
+    });
+
+    return (
+        <div className="user-input">
+            {/* user input + setTime, progress and accuracy. Missmatches to have highlight class added */}
+            <textarea id="userText"></textarea>
+        </div>
+    );
 };
