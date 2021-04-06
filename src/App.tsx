7f69978d7c3b787293fc60324d62d1ea33e1eec2
Jacek Roszkowiak <jacekroszkowiakdev@gmail.com>
2021-04-07 01:27:50 +0200

    Object is Possibly undefined problem solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { ProgressBar } from "./components/ProgressBar/";
+import { TypingPractice } from "./components/TypingPractice";
 import { useState, useEffect, createContext } from "react";
 
-export const TypingPracticeContext = createContext({
-    generatedText: null,
-    typingProgress: null,
-    startTime: null,
-});
+// export const TypingPracticeContext = createContext({
+//     generatedText: null,
+//     typingProgress: null,
+//     startTime: null,
+// });
+
+type ContextProps = {
+    generatedText: string;
+    // userText: string | null;
+    typingProgress: number;
+    startTime: number;
+};
+
+export const TypingPracticeContext = createContext<Partial<ContextProps>>({});
 
 function App() {
     const randomTextUrl = "https://litipsum.com//api/evelina/2/";
     const [isFetched, setIsFetched] = useState<boolean>(false);
     const [text, setText] = useState<string>();
+    const [userInput, setUserInput] = useState<string>();
     const [start, setStart] = useState<number>(0);
-    const [percentage, setPercentage] = useState<number>(0);
+    const [percentage, setPercentage] = useState<number>(0)!;
     const [error, setError] = useState<boolean>(false);
 
     const getAPIText = function () {
@@ -44,6 +55,7 @@ function App() {
             <TypingPracticeContext.Provider
                 value={{
                     generatedText: text,
+                    // userText: userInput,
                     typingProgress: percentage,
                     startTime: start,
                 }}
@@ -54,6 +66,7 @@ function App() {
                     <ProgressBar percentage={66} />
                     {text}
                     <button onClick={getAPIText}>LOAD</button>
+                    <TypingPractice />
                 </header>
             </TypingPracticeContext.Provider>
         </div>
--- src/components/TypingPractice/index.tsx
+++ src/components/TypingPractice/index.tsx
@@ -1,20 +1,43 @@
 import "./TypingPractice.css";
-import { useContext, useState } from "react";
+import { useContext, useState, useCallback } from "react";
 import { TypingPracticeContext } from "../../App";
 
-export const TypingPractice = () => {
-    console.log("typing practice component did mount");
-    const [time, setTime] = useState<number>(0);
-    const userText = document.getElementById("userText");
+// type TypingPracticeVariables = {
+//     userText: string | null;
+//     stringCheck: string | null;
+// };
 
-    userText.addEventListener("input", function () {
-        var userInput = userText.value;
-    });
+export const TypingPractice = () => {
+    const context = useContext(TypingPracticeContext);
+    const [text, setText] = useState<string>();
+    const [start, setStart] = useState<number>(0);
+    const userText = document.getElementById("userText");
+    const stringCheck = context.generatedText;
+
+    if (typeof context.generatedText === "string") {
+        console.log("stringCheck.length: ", stringCheck!.length);
+        console.log("stringCheck split: ", stringCheck!.split(""));
+    }
+
+    // console.log("stringCheck: ", stringCheck!.length);
+    // console.log("stringCheck split: ", stringCheck!.split(""));
+
+    //  userText.addEventListener("input", function () {
+    //      // console.log("logging input event");
+
+    //      var userInput = userText.value;
+    //      console.log("text:", userInput);
+    //      var swapInput = gettysburgAddress.substring(0, userInput.length);
+    //      userText.value = swapInput;
+    //  });
 
     return (
-        <div className="user-input">
-            {/* user input + setTime, progress and accuracy. Missmatches to have highlight class added */}
-            <textarea id="userText"></textarea>
+        <div className="TypingPracticeComponent">
+            <div className="user-input">
+                {/* user input + setTime, progress and accuracy. Mismatches to have highlight class added */}
+                <textarea id="userText"></textarea>
+            </div>
+            {context.generatedText}
         </div>
     );
 };
