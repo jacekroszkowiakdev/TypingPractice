bd730699f8ae9c699ce621bedd2f7154fa1525d0
Jacek Roszkowiak <jacekroszkowiakdev@gmail.com>
2021-04-09 02:57:12 +0200

    fixed timer flickering

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -6,14 +6,17 @@ export const Timer = () => {
 
     useEffect(() => {
         setStart(Date.now());
-        console.log("startTime: ", start);
-        setInterval(() => {
+
+        let timer = setInterval(() => {
+            console.log("A second has passed");
             setElapsedTime(getElapsedTime());
         }, 1000);
 
         const getElapsedTime = () => {
             return Math.floor((Date.now() - start) / 1000);
         };
+
+        return () => clearInterval(timer);
     }, [start]);
 
     return <div>Time: {elapsedTime!} seconds</div>;
